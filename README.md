7b35cdf731eaf97c1db778a880e70b0fab9c85f8
Guilherme Moreno <guilherme@pagolivre.com.br>
2020-01-22 08:17:18 -0300

    teste GuiaBolso

---
+++ README.md
@@ -0,0 +1,8 @@
+# Consumo-API-ChuckNorris
+Consumindo a API Chuck Norris Jokes.
+
+# 1º Passo
+npm install
+
+# 2º Passo
+npm start
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './components/Index';
+
+
+/*CSS*/
+import '.././public/css/bootstrap.min.css'
+import '.././public/css/style.css'
+
+ReactDOM.render(<Index />, document.getElementById('app'));
